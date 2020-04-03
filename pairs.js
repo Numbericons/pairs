1a1cf1b12cf68ccdea96c80c30297c5e8e805ab4
Zachary <zachjoliver@gmail.com>
2020-04-02 18:30:15 -0700

    Initial build and comments

---
+++ pairs.js
@@ -0,0 +1,13 @@
+//iterate across the array
+
+//option 1: nested array, first array stops 1 index before end
+
+//option 2:
+
+var buy = function(x, arr){
+  for (let i=0;i<arr.length;i++){
+    
+  }
+  
+  return null;
+};
