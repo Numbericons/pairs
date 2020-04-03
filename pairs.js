14f8b41286b241b235d9ba9e74b57c0eaa225bc3
Zachary <zachjoliver@gmail.com>
2020-04-02 18:35:40 -0700

    Change inner loop to instantiate to outer loop + 1

--- pairs.js
+++ pairs.js
@@ -4,7 +4,7 @@
 
 var buy = function (x, arr) {
   for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = 0; j < arr.length; j++) {
+    for (let j = i + 1; j < arr.length; j++) {
       if (arr[i] + arr[j] === x) return [i, j];
     }
   }
