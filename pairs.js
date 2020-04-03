5c6683712b60a247981b71b7ca0df06ac5f18035
Zachary <zachjoliver@gmail.com>
2020-04-02 18:33:09 -0700

    Added option 1 try

--- pairs.js
+++ pairs.js
@@ -2,12 +2,12 @@
 
 //option 1: nested array, first array stops 1 index before end
 
-//option 2:
-
-var buy = function(x, arr){
-  for (let i=0;i<arr.length;i++){
-    
+var buy = function (x, arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i] + arr[j] === x) return [arr[i], arr[j]];
+    }
   }
-  
+
   return null;
 };
