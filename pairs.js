2fc4d73d1b90d53ea3ab0077e68eeebb66a8d8ff
Zachary <zachjoliver@gmail.com>
2020-04-02 18:34:19 -0700

    Passing all but 1 example

--- pairs.js
+++ pairs.js
@@ -5,7 +5,7 @@
 var buy = function (x, arr) {
   for (let i = 0; i < arr.length - 1; i++) {
     for (let j = 0; j < arr.length; j++) {
-      if (arr[i] + arr[j] === x) return [arr[i], arr[j]];
+      if (arr[i] + arr[j] === x) return [i, j];
     }
   }
 
